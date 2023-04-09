da98048c96d15a6313c5799ddb1ce572e9f28122
Gustavo Soares <63077870+GusttavoSoares@users.noreply.github.com>
2023-04-09 19:12:54 +0000

    fix: remove comments

--- professors/models.py
+++ professors/models.py
@@ -12,9 +12,6 @@ class ProfessorManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
@@ -55,20 +52,11 @@ class Professors(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('professors')
 
     def get_full_name(self):
-        '''
-        Returns the first_name plus the last_name, with a space in between.
-        '''
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
-        '''
-        Returns the short name for the user.
-        '''
         return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
-        Sends an email to this User.
-        '''
         send_mail(subject, message, from_email, [self.email], **kwargs)
