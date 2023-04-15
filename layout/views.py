6b3adf0ae68a775ac746224ffe9a72f777a1fdd4
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-15 20:24:54 +0000

    feat: add render and classes in layout.view

--- layout/views.py
+++ layout/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
+from django.utils.translation import get_language, activate, gettext
 
 @login_required
 def index(request):
-    return render(request, "base.html", {})
+    trans = translate(language='pt-br')
+    return render(request, "login.html", {'trans': trans})
+
+def translate(language):
+    cur_language = get_language()
+    try:
+        activate(language)
+        text = gettext('login') #identifica o que mudar
+    finally:
+        activate(cur_language)
+    return text
