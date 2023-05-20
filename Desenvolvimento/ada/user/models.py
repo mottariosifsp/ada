a5a332b68cb7b876b6738181798e3d4aeb52edf8
isabella-66 <amasprog@gmail.com>
2023-05-20 19:22:48 -0300

    feat: add user models

--- Desenvolvimento/ada/user/models.py
+++ Desenvolvimento/ada/user/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+# Métodos de gerenciamento de usuário
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+    
+
+class Professors(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    registration_id = models.CharField(_('registration id'), max_length=9, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
+    telephone = models.CharField(_('telephone'), max_length=10)
+    cell_phone = models.CharField(_('cell phone'), max_length=14)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    password = models.CharField(_('password'), max_length=160)
+    is_superuser = models.BooleanField(_('superuser status'), default=False)
+    is_active = models.BooleanField(_('active'), default=True) #mudar depois
+    is_staff = models.BooleanField(_('staff status'), default=True)
+    
+    objects = ProfessorManager()
+
+    USERNAME_FIELD = 'promptuary'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('professor')
+        verbose_name_plural = _('professors')
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
