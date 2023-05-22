d6772239c7e7fcd290c7cc9b20ba19d122a2f970
Paulo Kenji <paulomuneischi@gmail.com>
2023-05-22 03:12:32 -0300

    feat: add get text on models (with team)

--- Desenvolvimento/ada/area/models.py
+++ Desenvolvimento/ada/area/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Area(models.Model):
-    name_area = models.CharField(max_length=45)
-    registration_area_id = models.CharField(max_length=30)
-    exchange_area = models.BooleanField(default=True)
-# Create your models here.
+    name_area = models.CharField(_('name area'), max_length=45)
+    registration_area_id = models.CharField(_('registration area id'), max_length=30)
+    exchange_area = models.BooleanField(_('exchange area'), default=True)
+
+    def __str__(self):
+        return self.name_area
--- Desenvolvimento/ada/course/models.py
+++ Desenvolvimento/ada/course/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from enums import enum
-
+from django.utils.translation import gettext_lazy as _
 
 class Course(models.Model):
-    id_course = models.AutoField(primary_key=True)
-    registration_course_id = models.CharField(max_length=45)
-    name_course = models.CharField(max_length=45)
-    period = models.CharField(choices=[(s.name, s.value) for s in enum.Period], max_length=45)
-    hour_start = models.TimeField()
-    hour_end = models.TimeField()
+    registration_course_id = models.CharField(_('registration course id'), max_length=45)
+    name_course = models.CharField(_('course name'), max_length=45)
+    period = models.CharField(_('period'), choices=[(s.name, s.value) for s in enum.Period], max_length=45)
+    hour_start = models.TimeField(_('start time'))
+    hour_end = models.TimeField(_('end time'))
+    area = models.ForeignKey('area.Area', on_delete=models.CASCADE, related_name='courses')
+
+    def __str__(self):
+        return self.name_course
+    
