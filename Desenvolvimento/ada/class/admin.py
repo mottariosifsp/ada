4c5e1ba2352186e594cc99ceb25c06753cf890f1
Paulo Kenji <paulomuneischi@gmail.com>
2023-05-22 05:04:38 -0300

    feat: add class admin

--- Desenvolvimento/ada/class/admin.py
+++ Desenvolvimento/ada/class/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
-# Register your models here.
+from .models import Class
+
+class Class_admin(admin.ModelAdmin):
+    list_display = ('registration_class_id', 'period', 'semester', 'is_high_school', 'area')
+    search_fields = ('registration_class_id', 'semester')
+
+admin.site.register(Class, Class_admin)
+# Register your models here
