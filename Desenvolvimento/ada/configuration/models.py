4cfdb9dd53889767c84d0ddc4f67beabee69c3d6
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-05-23 17:40:14 -0300

    feat: add criteria validation (with ana)

--- Desenvolvimento/ada/configuration/models.py
+++ Desenvolvimento/ada/configuration/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Deadline(models.Model):
     name = models.CharField(('name'), max_length=90)
@@ -8,3 +9,11 @@ class Deadline(models.Model):
 class Criteria(models.Model):
     name_criteria = models.CharField(('name criteria'), max_length=45)
     is_select = models.BooleanField(('is selected'), default=False)
+
+    def clean(self):
+        if self.is_select and Criteria.objects.filter(is_select=True).exists():
+            raise ValidationError('Only one Criteria can be selected at a time.')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Run validation before saving
+        super().save(*args, **kwargs)
