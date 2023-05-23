b978c93bb9a7369d55eb1a7e1dbc87b9c1787658
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-23 03:08:14 -0300

    feat: add deadline model

--- Desenvolvimento/ada/configuration/models.py
+++ Desenvolvimento/ada/configuration/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-
+class Deadline(models.Model):
+    name = models.CharField(_('name'), max_length=90)
+    daedline_start = models.DateTimeField(_('deadline start'))
+    deadline_end = models.DateTimeField(_('deadline end'))
