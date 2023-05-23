dc803371fb237a97cffea9cdad1e70332635e9d6
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-23 03:11:53 -0300

    feat: add deadline admin (with paulo and isabella)

--- Desenvolvimento/ada/configuration/admin.py
+++ Desenvolvimento/ada/configuration/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Deadline
 
-# Register your models here.
+class Deadline_admin(admin.ModelAdmin):
+    list_display = ('name','deadline_start','deadline_end')
+    search_fields = ('name',)
