7fec0ad0a8bbfb1135132dd1e2a2fcb2339898e8
isabella-66 <amasprog@gmail.com>
2023-05-23 23:11:07 -0300

    fix: validate_start_greater_end and implement in timeslot model (with josineudo e paulo)

--- Desenvolvimento/ada/common/validator/validator.py
+++ Desenvolvimento/ada/common/validator/validator.py
@@ -1,13 +1,16 @@
 from django.core.exceptions import ValidationError
-from datetime import time
 
 def validate_uppercase(value):
     if value != value.upper():
         raise ValidationError("Este campo deve conter apenas letras maiúsculas.")
+
+def validate_start_greater_end(value):
+    if value.hour_start > value.hour_end:
+        raise ValidationError("O horário de fim ocorre antes do início.")
 
 def validate_interrupted_time(model, value):
     objects = model.objects.all()
 
     for obj in objects:
         if obj.hour_start < value.hour_start < obj.hour_end or obj.hour_start < value.hour_end < obj.hour_end:
-            raise ValidationError('O horário se sobrepõe a outro horário existente.')
+            raise ValidationError("O horário se sobrepõe a outro horário existente.")
