170383cb00bdaef48c5aeba9da943391df9df24e
isabella-66 <amasprog@gmail.com>
2023-05-27 06:01:31 -0300

    fix: merge conflict in area admin

--- Desenvolvimento/ada/area/admin.py
+++ Desenvolvimento/ada/area/admin.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 from .models import Area, Block
 
 class Area_admin(admin.ModelAdmin):
-<<<<<<< HEAD
-    list_display = ('name_area', 'registration_area_id', 'exchange_area', 'is_high_school')
-=======
-    list_display = ('name_area', 'registration_area_id', 'exchange_area', 'block_names')
->>>>>>> 0cd878babadd709af1672b2ec4748af38f141080
+    list_display = ('name_area', 'registration_area_id', 'exchange_area', 'block_names', 'is_high_school')
     search_fields = ('name_area', 'registration_area_id')
 
     def block_names(self, obj):
