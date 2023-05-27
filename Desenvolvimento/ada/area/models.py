e5c1d63a483c8232a09aae47e8550411215d5265
isabella-66 <amasprog@gmail.com>
2023-05-27 05:51:08 -0300

    fix: max length area model

--- Desenvolvimento/ada/area/models.py
+++ Desenvolvimento/ada/area/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Area(models.Model):
-    name_area = models.CharField(_('name area'), max_length=45)
+    name_area = models.CharField(_('name area'), max_length=60)
     registration_area_id = models.CharField(_('registration area id'), max_length=20)
     exchange_area = models.BooleanField(_('exchange area'), default=True)
     blocks = models.ManyToManyField('Block', blank=True)
