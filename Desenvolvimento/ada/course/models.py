4e98cb1d98d1b52b969b6bca34c473fdf7c5d579
isabella-66 <amasprog@gmail.com>
2023-05-27 05:56:41 -0300

    fix: max length course model

--- Desenvolvimento/ada/course/models.py
+++ Desenvolvimento/ada/course/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Course(models.Model):
     id_course = models.AutoField(primary_key=True)
     registration_course_id = models.CharField(_('registration course id'), max_length=20)
-    name_course = models.CharField(_('course name'), max_length=45)
+    name_course = models.CharField(_('course name'), max_length=160)
     period = models.CharField(_('period'), choices=[(s.name, s.value) for s in enum.Period], max_length=45)
     hour_start = models.TimeField(_('start time'))
     hour_end = models.TimeField(_('end time'))
