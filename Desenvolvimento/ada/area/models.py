97730d13dde12e15a72a2e66c05ee1596b439641
isabella-66 <amasprog@gmail.com>
2023-05-27 05:52:48 -0300

    fix: max length block model

--- Desenvolvimento/ada/area/models.py
+++ Desenvolvimento/ada/area/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Area(models.Model):
-    name_area = models.CharField(_('name area'), max_length=60)
+    name_area = models.CharField(_('name area'), max_length=90)
     registration_area_id = models.CharField(_('registration area id'), max_length=20)
     exchange_area = models.BooleanField(_('exchange area'), default=True)
     blocks = models.ManyToManyField('Block', blank=True)
@@ -14,7 +14,7 @@ class Area(models.Model):
 
 class Block(models.Model):
     registration_block_id = models.AutoField(primary_key=True)
-    name_block = models.CharField(_('name block'), max_length=45)
+    name_block = models.CharField(_('name block'), max_length=90)
 
     def __str__(self):
         return self.name_block
