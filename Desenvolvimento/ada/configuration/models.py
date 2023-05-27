489458e76897655a64cb0a50728b6c0ad55fde04
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-27 05:37:14 -0300

    feat: add internacionalization model configuration

--- Desenvolvimento/ada/configuration/models.py
+++ Desenvolvimento/ada/configuration/models.py
@@ -4,3 +4,7 @@ class Deadline(models.Model):
     name = models.CharField(_('name'), max_length=90)
     daedline_start = models.DateTimeField(_('deadline start'))
     deadline_end = models.DateTimeField(_('deadline end'))
+
+    class Meta:
+        verbose_name = _('deadline')
+        verbose_name_plural = _('deadlines')
