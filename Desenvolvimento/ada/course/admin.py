bd315b51b51c96b44c8415094329ad514cb48740
isabella-66 <amasprog@gmail.com>
2023-05-27 22:43:31 -0300

    reload: migrations

--- Desenvolvimento/ada/course/admin.py
+++ Desenvolvimento/ada/course/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Course
 
 class Course_admin(admin.ModelAdmin):
-    list_display = ('id_course','registration_course_id','name_course', 'acronym', 'area', 'period', 'hour_start', 'hour_end')
-    search_fields = ('id_course','registration_course_id','name_course', 'acronym', 'period', 'hour_start', 'hour_end')
+    list_display = ('id_course','registration_course_id','name_course', 'acronym', 'area', 'block')
+    search_fields = ('id_course','registration_course_id','name_course', 'acronym')
 
 admin.site.register(Course, Course_admin)
--- Desenvolvimento/ada/course/models.py
+++ Desenvolvimento/ada/course/models.py
@@ -8,9 +8,6 @@ class Course(models.Model):
     registration_course_id = models.CharField(_('registration course id'), max_length=20, unique=True)
     name_course = models.CharField(_('course name'), max_length=45, validators=[validate_uppercase])
     acronym = models.CharField(_('acronym'), max_length=3, null=True, validators=[validate_uppercase, validate_acronym_length])
-    period = models.CharField(_('period'), choices=[(s.name, s.value) for s in enum.Period], max_length=45)
-    hour_start = models.TimeField(_('start time'))
-    hour_end = models.TimeField(_('end time'))
     area = models.ForeignKey('area.Area', on_delete=models.CASCADE, null=True)
     block = models.ForeignKey('area.Block', on_delete=models.CASCADE, null=True)
 
