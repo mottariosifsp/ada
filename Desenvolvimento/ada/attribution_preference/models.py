b60c33b01b809d595d45e28529156a775c1d27d2
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-05-28 00:00:12 -0300

    fix: delete unnecessary tables

--- Desenvolvimento/ada/attribution_preference/models.py
+++ Desenvolvimento/ada/attribution_preference/models.py
@@ -18,38 +18,6 @@ class Preference_schedule(models.Model):
         verbose_name = _('preference_schedule')
         verbose_name_plural = _('preference_schedules')
 
-class Teaching_support_activity_attribution(models.Model):
-    attribution_preference = models.ForeignKey('Attribution_preference', on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = _('teaching_support_activity_attribution')
-        verbose_name_plural = _('teaching_support_activity_attributions')
-
-class Teaching_support_activity(models.Model):
-    name = models.CharField(_('name'), max_length=45)
-    duration = models.TimeField(_('duration'))
-
-    class Meta:
-        verbose_name = _('teaching_support_activity')
-        verbose_name_plural = _('teaching_support_activities')
-
-class Attribution_workload_supplementation(models.Model):
-    attribution_preference = models.ForeignKey('Attribution_preference', on_delete=models.CASCADE)
-    workload_supplementation = models.ForeignKey('Workload_supplementation', on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = _('attribution_workload_supplementation')
-        verbose_name_plural = _('attribution_workload_supplementations')
-
-class Workload_supplementation(models.Model):
-    name = models.CharField(_('name'), max_length=45)
-    duration = models.TimeField(_('duration'))
-
-    class Meta:
-        verbose_name = _('workload_supplementation')
-        verbose_name_plural = _('workload_supplementations')
-
-
 class Attribution_preference_course_preference(models.Model):
     attribution_preference = models.ForeignKey('Attribution_preference', on_delete=models.CASCADE)
     course_preference = models.ForeignKey('Course_preference', on_delete=models.CASCADE)
