4110336bc03d51c42a2dc385e0f75220cb51b135
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-05-28 20:55:13 -0300

    fix: queue view

--- Desenvolvimento/ada/attribution/views.py
+++ Desenvolvimento/ada/attribution/views.py
@@ -3,105 +3,65 @@ from user.models import User
 from attribution.models import TeacherQueuePosition
 from django.shortcuts import render, redirect
 import json
-from django.db.models import Sum
-from django.db.models import Subquery, Sum, OuterRef
-from django.db.models.query import QuerySet
 from django.db.models import F, Sum, Value
 
 marcador = 0
 tabela_data = ""
 
-# Lógica para separar em áreas
-# fazer filtro somente com os professores de uma determinada área
-
-# Seleciona as áreas que o admin pertence
-# def get_areas(request):
-#         user = request.user
-#         blocks = user.blocks.all()
-#         areas = []
-#         for block in blocks:
-#             areas += block.areas.all()
-#         # areas = [area for block in blocks for area in block.areas.all()] - colocar
-#         return areas
-
 # Seleciona o campo marcado pelo administrador e verifica qual é o atributo correspondente no histórico dos usuários
 def get_selected_campo():
     if Criteria.objects.filter(is_select=True).exists():
         valor_numero = Criteria.objects.filter(is_select=True).values('number_criteria').first().get('number_criteria')
-        print("Criterio escolhido pelo adm", valor_numero)
-        campos = {
-            1: 'birth',
-            2: 'date_career',
-            3: 'date_campus',
-            4: 'date_professor',
-            5: 'date_area',
-            6: 'date_institute',
-            7: 'academic_degrees'
-        }
 
-        return campos.get(valor_numero, "campo")
+        if valor_numero in range(1, 8): # 1-7, exclui o 8
+            campos = {
+                1: 'birth',
+                2: 'date_career',
+                3: 'date_campus',
+                4: 'date_professor',
+                5: 'date_area',
+                6: 'date_institute',
+                7: 'academic_degrees'
+            }
 
-# se nenhum critério foi selecionado para o adm retorna um campo em branco
+            return campos.get(valor_numero, "campo")
+        else:
+            return ""
     else:
         return ""
 
-# metódo que adiciona os usuários na na fila (TeacherQueuePosition)
-# vai ter algum problema quando tiver mais de uma fila para diferentes áreas ao memso tempo (como vai filtrar? 2 positions?)
 def add_teacher_to_queue(teacher, position_input):
     position = position_input
     TeacherQueuePosition.objects.create(teacher=teacher, position=position)
 
 # View que leva para a(s) fila(s) já definida pelo admin
 def queue(request):
-        campo = get_selected_campo()
-        # areas = get_areas(request)
+    campo = get_selected_campo()
 
-        resultados = TeacherQueuePosition.objects.select_related('teacher').order_by('position').all()
+    teacher_positions = TeacherQueuePosition.objects.order_by('position')
 
-        usuarios_somados = User.objects.annotate(
-            total_score=Subquery(
-                TeacherQueuePosition.objects.filter(teacher__id=OuterRef('id')).values('teacher__id').annotate(
-                    total=Sum('teacher__history__academic_degrees__punctuation')
-                ).values('total')
-            )
-        ).values('registration_id', 'total_score')
+    total_scores = []
 
+    for teacher_position in teacher_positions:
+        user = teacher_position.teacher
+        history = user.history
+        total_score = history.academic_degrees.aggregate(total_score=Sum('punctuation'))['total_score']
+        total_scores.append(total_score)
 
-        data = {
-            'criterios': Criteria.objects.all(),
-            'resultados': resultados,
-            'total_score': usuarios_somados,
-            'marcadorDiff': 0,
-            'campo': campo,
-        }
+    resultados = teacher_positions.select_related('teacher').order_by('position').all()
 
-        # precisa fazer a lógica para ver filas diferentes
+    data = {
+        'resultados': resultados,
+        'total_scores': total_scores,
+        'campo': campo,
+    }
 
-        print(1)
-
-        return render(request, 'attribution/queue.html', {'data': data})
+    return render(request, 'attribution/queue.html', {'data': data})
 
 # View que leva para a página de definir a fila
 def queueSetup(request):
     global marcador
     global tabela_data # variável utilizada caso a fila já tenha sido definida pelo menos uma vez pelo admin
-
-    # if request.method == 'GET' and 'area' in request.GET:
-    #     selected_area = request.GET.get('area')
-    #     resultados = User.objects.filter(blocks__areas__name_area=selected_area)
-    #     # areas = get_areas(request)
-    #
-    #     campo = get_selected_campo()
-    #
-    #     data = {
-    #         'resultados': resultados,
-    #         'marcadorDiff': 0,
-    #         'campo': campo,
-    #         # 'areas': areas
-    #     }
-
-        # print("aqui22")
-        # return render(request, 'attribution/queueSetup.html', {'data': data})
 
     if request.method == 'POST': # adiciona os professores no model TeacherQueuePosition
         marcador = 1; # marcador fica como 1 para ter o controle que já foi criado uma tabela
@@ -121,12 +81,8 @@ def queueSetup(request):
 
         data = {
             'resultados': TeacherQueuePosition.objects.select_related('teacher').order_by('position').all(),
-            'marcadorDiff': '1', # serve para diferenciar se vai ser user (antes de enviar a tabela) ou teacher no html
             'campo': campo,
-            # 'area': get_areas(request)
         }
-
-        print(2)
 
         return render(request, 'attribution/queueSetup.html', {'data': data})
 
@@ -143,58 +99,34 @@ def queueSetup(request):
                 if not teacher_positions.filter(teacher=user).exists():
                     missing_users.append(user)
 
-            final_list = list(teacher_positions) + missing_users
+                final_list = list(teacher_positions) + missing_users
 
-            usuarios_somados = []
+                usuarios_somados = []
 
-            for item in final_list:
-                if isinstance(item, TeacherQueuePosition):
-                    user = item.teacher
-                    if user is not None and user.history is not None:
-                        total_score = user.history.academic_degrees.aggregate(total_score=Sum('punctuation'))[
-                            'total_score']
+                for item in final_list:
+                    if isinstance(item, TeacherQueuePosition):
+                        user = item.teacher
+                        if user is not None and user.history is not None:
+                            total_score = user.history.academic_degrees.aggregate(total_score=Sum('punctuation'))[
+                                'total_score']
+                        else:
+                            total_score = 0
                     else:
-                        total_score = 0
-                else:
-                    user = item
-                    if user is not None and user.history is not None:
-                        # teacher_positions_count = TeacherQueuePosition.objects.count()
-                        total_score = user.history.academic_degrees.aggregate(total_score=Sum('punctuation'))[
-                            'total_score'] # Ok, está funcionando - só precisa add logica no template
-                    else:
-                        total_score = 0
-                user.total_score = total_score
-                usuarios_somados.append(user)
+                        user = item
+                        if user is not None and user.history is not None:
 
-
-            usuarios_somados = list(usuarios_somados)
-
-            print("USUARIOS SOMADOAS", usuarios_somados)
-
-            if isinstance(final_list[0], TeacherQueuePosition):
-                # Se o primeiro item for um TeacherQueuePosition
-                item = final_list[0]
-                print("Atributo do TeacherQueuePosition:", item.teacher.first_name)
-
-            # areas = get_areas(request)
-
-            print("Posição 0", usuarios_somados)
-
-            print("oiii777")
-
-            print(final_list)
+                            total_score = user.history.academic_degrees.aggregate(total_score=Sum('punctuation'))[
+                                'total_score']
+                        else:
+                            total_score = 0
+                    user.total_score = total_score
+                    usuarios_somados.append(user)
 
             data = {
                 'resultados': final_list,
-                'marcadorDiff': '1',
                 'campo': campo,
                 'total_score': usuarios_somados,
-                'tem_novo_user': 1,
-                # 'areas': areas,
-                # 'posicao_do_ultimo_usuario': teacher_positions_count
             }
-
-            print(3)
 
             return render(request, 'attribution/queueSetup.html', {'data': data})
 
@@ -205,46 +137,42 @@ def queueSetup(request):
 
                 usuarios_ordenados = User.objects.all().order_by(f'history__{campo}')
 
-                # Anotar a soma das pontuações para cada usuário
+                # Faz a soma dos academic degrees para cada usuário
                 usuarios_somados = usuarios_ordenados.annotate(total_score=Sum('history__academic_degrees__punctuation'))
-
-                for usuario in usuarios_somados:
-                    print("Usuário:", usuario.get_full_name())
-                    print("Pontuação total:", usuario.total_score)
-                    print("------------------------------")
 
                 data = {
                     'resultados': usuarios_ordenados,
-                    'marcadorDiff': 0,
                     'campo': campo,
                     'total_score': usuarios_somados,
-                    # 'areas': areas
                 }
 
-                print(4)
                 return render(request, 'attribution/queueSetup.html', {'data': data})
+
             else: # se o superadmin selecionou um critério que não tenha relação com nenhum atributo do histórico vai cair aqui
+                  # fazer exception?
 
-                resultados = User.objects.all()
-                #validar por area
+                usuarios_ordenados = User.objects.all()
+
+                usuarios_somados = usuarios_ordenados.annotate(total_score=Sum('history__academic_degrees__punctuation'))
+
                 data = {
-                    'resultados': resultados,
-                    'marcadorDiff': 0,
-                    # 'campo': "Esse critério não corresponde a nenhum atributo do histórico do usuário"
+                    'resultados': usuarios_ordenados,
+                    'campo': campo,
+                    'total_score': usuarios_somados,
                 }
+
                 return render(request, 'attribution/queueSetup.html', {'data': data})
 
-        resultados = User.objects.all() # se nenhum critério foi selecionado pelo adm e não tiver feito nenhuma lista manual vai cair aqui
+        # se nenhum critério foi selecionado pelo adm e não tiver feito nenhuma lista manual vai cair aqui
         campo = get_selected_campo()
-        # areas = get_areas(request)
+
+        usuarios_ordenados = User.objects.all()
+        usuarios_somados = usuarios_ordenados.annotate(total_score=Sum('history__academic_degrees__punctuation'))
 
         data = {
-            'resultados': resultados,
-            'marcadorDiff': 0,
+            'resultados': usuarios_ordenados,
             'campo': campo,
-            # 'areas': areas
+            'total_score': usuarios_somados,
         }
-
-        print(5)
 
         return render(request, 'attribution/queueSetup.html', {'data': data})
