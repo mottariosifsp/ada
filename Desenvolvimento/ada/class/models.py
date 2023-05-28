9b309f7b5168e54ec262397b23615c8906fa333b
isabella-66 <amasprog@gmail.com>
2023-05-28 01:20:39 -0300

    feat: add unique to registration class id in class model

--- Desenvolvimento/ada/class/models.py
+++ Desenvolvimento/ada/class/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from enums import enum
 
 class Class(models.Model):
-    registration_class_id = models.CharField(_('registration class id'), max_length=20)
+    registration_class_id = models.CharField(_('registration class id'), max_length=20, unique=True)
     period = models.CharField(_('period'), choices=[(s.name, s.value) for s in enum.Period], max_length=45)
     semester = models.IntegerField(_('semester'))
     area = models.ForeignKey('area.Area', on_delete=models.CASCADE, null=True)
