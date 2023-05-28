1466e5d4fdd0da6c4b82304439d57b75af5aec18
Paulo Kenji <paulomuneischi@gmail.com>
2023-05-28 02:45:55 -0300

    feat: Add function to assign class position based on its schedule

--- Desenvolvimento/ada/common/processors.py
+++ Desenvolvimento/ada/common/processors.py
@@ -2,3 +2,11 @@ def convert_to_uppercase(sender, instance, value):
     field_value = getattr(instance, sender.value)
     uppercase_value = field_value.upper()
     setattr(instance, sender.uppercase_field, uppercase_value)
+
+def sortByTime(model):
+    count = 1
+    for object in model.objects.all().order_by('hour_start'):
+        object.position = count
+        count += 1 
+        object.save()
+    print(model.objects.order_by('hour_start')[0].position)
--- Desenvolvimento/ada/user/admin.py
+++ Desenvolvimento/ada/user/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+class User_admin(admin.ModelAdmin):
+    readonly_fields = ('password',)
+    list_display = ('registration_id', 'first_name', 'email',)
+    search_fields = ('registration_id', 'first_name', 'email',)
+
+admin.site.register(User, User_admin)
