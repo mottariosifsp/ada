d38bb9b07261febbe27034d7a8b9139e65613150
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-05-29 01:08:38 -0300

    feat: add criteria in staff model (with ana paula)

--- Desenvolvimento/ada/configuration/tasks.py
+++
@@ -1,5 +0,0 @@
-from celery import Celery, shared_task
-
-app = Celery('tasks', broker='redis://localhost:6379')
-
-app.conf.timezone = 'UTC'
--- Desenvolvimento/ada/staff/admin.py
+++ Desenvolvimento/ada/staff/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Deadline
+from .models import Deadline, Criteria
 
 class Deadline_admin(admin.ModelAdmin):
     list_display = ('name','deadline_start','deadline_end')
     search_fields = ('name',)
 
 admin.site.register(Deadline, Deadline_admin)
+
+class Criteria_admin(admin.ModelAdmin):
+    list_display = ('name_criteria','number_criteria','is_select')
+    search_fields = ('name_criteria',)
+
+admin.site.register(Criteria, Criteria_admin)
