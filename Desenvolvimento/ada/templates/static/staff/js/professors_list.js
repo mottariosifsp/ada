21e28d194a63d60ae7c8d1bd3f6174a2ef78160f
isabella-66 <amasprog@gmail.com>
2023-05-29 05:15:57 -0300

    feat: add modal to professors list (with ana paula, gustavo, josineudo and paulo)

--- Desenvolvimento/ada/templates/static/staff/js/professors_list.js
+++ Desenvolvimento/ada/templates/static/staff/js/professors_list.js
@@ -1,6 +1,18 @@
+// Carregar DataTable
+
 $(document).ready( function () {
     $('#professors_list').DataTable();
 } );
 let table = new DataTable('#professors_list', {
     responsive: true
+});
+
+// Modal
+
+$(document).on('click', '[data-dismiss="modal"]', function() {
+    $('#courseName').val('');
+    $('#numClasses').val('');
+    $('input[name="priority"]').prop('checked', false);
+    $('#period').val('');
+    $('#error-alert').hide();
 });
