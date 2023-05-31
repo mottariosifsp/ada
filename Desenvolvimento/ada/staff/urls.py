04dfac10a3434cf5450e25beebaba51da76cb30f
isabella-66 <amasprog@gmail.com>
2023-05-31 02:13:17 -0300

    feat: add list classes in classes list

--- Desenvolvimento/ada/staff/urls.py
+++ Desenvolvimento/ada/staff/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("cadastrar-prazos/", deadline_configuration, name="deadline_configuration"),
     path("prazo-cadastrado/", confirm_deadline_configuration, name="confirm_deadline_configuration"),
     path("prazo-atual/", show_current_deadline, name="show_current_deadline"),
-    path("listar-professores/", professors_list, name="professors_list"),
+    path("professores/", professors_list, name="professors_list"),
     path("alteracoes-salvas/", update_save, name="update_save"),
-    path("listar_turmas/", classes_list, name="classes_list"),
+    path("turmas/", classes_list, name="classes_list"),
 ]
