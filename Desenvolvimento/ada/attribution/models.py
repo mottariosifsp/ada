88479e8bade4ded7681d6baacf9184431205a592
isabella-66 <amasprog@gmail.com>
2023-05-31 22:01:19 -0300

    merge: fix conflicts

--- Desenvolvimento/ada/attribution/models.py
+++ Desenvolvimento/ada/attribution/models.py
@@ -1,6 +1,5 @@
 from user.models import User
 from django.db import models
-<<<<<<< HEAD
 from django.core.exceptions import ValidationError
 
 class TeacherQueuePosition(models.Model):
@@ -13,5 +12,3 @@ class TeacherQueuePosition(models.Model):
     def clean(self):
         if self.position <= 0:
             raise ValidationError("Deve ser maior que 0")
-=======
->>>>>>> style/superadmin-change
