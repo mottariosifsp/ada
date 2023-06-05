ef86cc689a310a7edc4cc511e8db0585847af8f3
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-06-05 10:06:40 -0300

    feat: new validations in countt hours and availibilty user

--- Desenvolvimento/ada/attribution_preference/views.py
+++ Desenvolvimento/ada/attribution_preference/views.py
@@ -31,44 +31,41 @@ def attribution_preference(request):
 
 def confirm_attribution_preference(request):
     work_regime = request.POST.get('work_regime')
-    preferred_courses = request.POST.getlist('work_courses[]')
 
     if request.method == 'POST':
         data = {
-            'work_regime': work_regime,
-            'preferred_courses': preferred_courses
+            'work_regime': work_regime
         }
         print(data)
-        save_course_preference(preferred_courses, request)
+        # save_course_preference(preferred_courses, request)
         print("sim")
 
         return render(request, 'attribution_preference/confirm_attribution_preference.html')
     elif request.method == 'GET':
-        courses = Course_preference.objects.all()
         data = {
-            'preferred_courses': courses
+            'preferred_courses': "baata"
         }
         print(data)
 
         return render(request, 'attribution_preference/confirm_attribution_preference.html', data)
 
-@transaction.atomic
-def save_course_preference(preferred_courses, request):
-    for course in preferred_courses:
-        if Course_preference.objects.filter(attribution_preference_course_preference_attribution_preference_user=request.user).exists():
-            Course_preference.objects.filter(attribution_preference_course_preference_attribution_preference_user=request.user).delete() 
-            Course_preference.objects.create(
-            course=Course.objects.filter(name_course=course.nome), 
-            course_name=course.nome,
-            count_course=data[course.numAulas], 
-            priority=course.prioridade,
-            period=course.turno
-            )
-        else:
-            Course_preference.objects.create(
-            course=Course.objects.filter(name_course=course.nome), 
-            course_name=course.nome,
-            count_course=data[course.numAulas], 
-            priority=course.prioridade,
-            period=course.turno
-            )
+#@transaction.atomic
+#def save_course_preference(preferred_courses, request):
+#    for course in preferred_courses:
+#        if Course_preference.objects.filter(attribution_preference_course_preference_attribution_preference_user=request.user).exists():
+#            Course_preference.objects.filter(attribution_preference_course_preference_attribution_preference_user=request.user).delete() 
+#            Course_preference.objects.create(
+#            course=Course.objects.filter(name_course=course.nome), 
+#            course_name=course.nome,
+#            count_course=data[course.numAulas], 
+#            priority=course.prioridade,
+#            period=course.turno
+#            )
+#        else:
+##            Course_preference.objects.create(
+#            course=Course.objects.filter(name_course=course.nome), 
+#            course_name=course.nome,
+ #           count_course=data[course.numAulas], 
+ #           priority=course.prioridade,
+ #           period=course.turno
+ #           )
