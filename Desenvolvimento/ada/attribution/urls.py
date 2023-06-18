43e2cb46e094f31cc16649030c6367e7c9923470
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-18 00:08:34 -0300

    feat: manual attribution in url

--- Desenvolvimento/ada/attribution/urls.py
+++ Desenvolvimento/ada/attribution/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from attribution.views import attribution, email_test
+from attribution.views import attribution, email_test, manual_attribution
 
 app_name = 'attribution'
 
 urlpatterns = [
     path('atribuicao-de-aulas/', attribution, name='attribution'),
+    path('atribuicao-manual/', manual_attribution, name='manual_attribution'),
     path('email_test/', email_test, name='email_test')
 ]
