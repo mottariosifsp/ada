a7468c8db8e7b11cf1a24e17d0df8fb9b0931999
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-19 04:31:59 -0300

    feat: attribution detail pages
    Co-authored-by: Josineudo Arruda <josineudo-arruda@users.noreply.github.com>

--- Desenvolvimento/ada/attribution/urls.py
+++ Desenvolvimento/ada/attribution/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from attribution.views import attribution, email_test, manual_attribution
+from attribution.views import attribution, email_test, manual_attribution, attribution_detail
 
 app_name = 'attribution'
 
 urlpatterns = [
     path('atribuicao-de-aulas/', attribution, name='attribution'),
     path('atribuicao-manual/', manual_attribution, name='manual_attribution'),
-    path('email_test/', email_test, name='email_test')
+    path('email_test/', email_test, name='email_test'),
+    path("atribuicao-blocos/<str:registration_block_id>/", attribution_detail, name="attribution_detail"),
 ]
