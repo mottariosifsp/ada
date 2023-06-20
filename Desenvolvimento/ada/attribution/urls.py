4f87e9a2c7d33d781e91898e79866e70e58f0d93
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-20 00:28:28 -0300

    feat: manual attribution confirm url

--- Desenvolvimento/ada/attribution/urls.py
+++ Desenvolvimento/ada/attribution/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from attribution.views import attribution, email_test, manual_attribution
+from attribution.views import attribution, email_test, manual_attribution, manual_attribution_confirm
 
 app_name = 'attribution'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('atribuicao-de-aulas/', attribution, name='attribution'),
     path('atribuicao-manual/', manual_attribution, name='manual_attribution'),
     path('email_test/', email_test, name='email_test'),
+    path('atribuicao-manual-confirmar/', manual_attribution_confirm, name='manual_attribution_confirm'),
     # path("atribuicao-blocos/<str:registration_block_id>/", attribution_detail, name="attribution_detail"),
 ]
