ce30444ab98b8da7c03f806695a3e6d846500fe9
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-21 00:13:16 -0300

    feat: profile page

--- Desenvolvimento/ada/professor/urls.py
+++ Desenvolvimento/ada/professor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import home
+from .views import home, profile
 
 urlpatterns = [
     path("", home, name="home_professor"),
-    path("preferencia-atribuicao/", include("attribution_preference.urls"))
+    path("preferencia-atribuicao/", include("attribution_preference.urls")),
+    path("perfil/", profile, name="profile"),
 ]
