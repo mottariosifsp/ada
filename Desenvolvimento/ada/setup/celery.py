a935c8966922535ab1403aa6cd528a777fd08525
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-25 02:48:40 -0300

    fix: celery configuration

--- Desenvolvimento/ada/setup/celery.py
+++ Desenvolvimento/ada/setup/celery.py
@@ -2,6 +2,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "setup.settings")
-app = Celery("ada")
+app = Celery("ada", broker_connection_retry_on_startup = True)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
