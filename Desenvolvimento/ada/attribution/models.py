379383c193240717af88259d74f70a8b24653f90
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-25 04:21:24 -0300

    feat: add teacher queue backup model

--- Desenvolvimento/ada/attribution/models.py
+++ Desenvolvimento/ada/attribution/models.py
@@ -16,6 +16,18 @@ class TeacherQueuePosition(models.Model):
         if self.position <= 0:
             raise ValidationError("Deve ser maior que 0")
 
+class TeacherQueuePositionBackup(models.Model):
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    position = models.IntegerField()
+    blockk = models.ForeignKey(Blockk, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.teacher} at position {self.position}"
+
+    def clean(self):
+        if self.position <= 0:
+            raise ValidationError("Deve ser maior que 0")
+
 @receiver(models.signals.pre_save, sender=User)
 def on_change(sender, instance, **kwargs): 
     old_instance = sender.objects.get(pk=instance.pk)
