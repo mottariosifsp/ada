aaf512fe4071c9a194778a36b5d5f5731ea6fa36
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-25 04:38:54 -0300

    feat: ordering to TeacherQueuePosition models

--- Desenvolvimento/ada/attribution/admin.py
+++ Desenvolvimento/ada/attribution/admin.py
@@ -4,10 +4,13 @@ from .models import TeacherQueuePosition, TeacherQueuePositionBackup
 class TeacherQueuePosition_admin(admin.ModelAdmin):
     list_display = ('teacher', 'position', 'blockk')
     search_fields = ('teacher', 'position', 'blockk')
+    ordering = ['position']
 
 class TeacherQueuePositionBackup_admin(admin.ModelAdmin):
     list_display = ('teacher', 'position', 'blockk')
     search_fields = ('teacher', 'position', 'blockk')
+    ordering = ['position']
+    
 
 admin.site.register(TeacherQueuePosition, TeacherQueuePosition_admin)
 admin.site.register(TeacherQueuePositionBackup, TeacherQueuePositionBackup_admin)
