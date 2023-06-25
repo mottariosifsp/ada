bd08536153f5989fcc5dc9852e2e5c29687967e8
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-25 04:33:29 -0300

    feat: add teacher queue backup in admin page

--- Desenvolvimento/ada/attribution/admin.py
+++ Desenvolvimento/ada/attribution/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import TeacherQueuePosition
+from .models import TeacherQueuePosition, TeacherQueuePositionBackup
 
 class TeacherQueuePosition_admin(admin.ModelAdmin):
     list_display = ('teacher', 'position', 'blockk')
     search_fields = ('teacher', 'position', 'blockk')
 
+class TeacherQueuePositionBackup_admin(admin.ModelAdmin):
+    list_display = ('teacher', 'position', 'blockk')
+    search_fields = ('teacher', 'position', 'blockk')
+
 admin.site.register(TeacherQueuePosition, TeacherQueuePosition_admin)
+admin.site.register(TeacherQueuePositionBackup, TeacherQueuePositionBackup_admin)
