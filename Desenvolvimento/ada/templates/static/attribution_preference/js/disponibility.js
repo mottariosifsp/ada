65e03e5480b9dcf36375a7810d715f9e0c461fe1
ana004 <paulaana08002019@gmail.com>
2023-07-17 13:53:34 -0300

    feat: add part of logic for 8 work hours rule (with josineudo arruda's help)

--- Desenvolvimento/ada/templates/static/attribution_preference/js/disponibility.js
+++ Desenvolvimento/ada/templates/static/attribution_preference/js/disponibility.js
@@ -2,11 +2,14 @@ var current_language = document.currentScript.getAttribute('data-lang');
 var timeslots = []
 var cell_left_number = 0
 var cell_type_choosed = 0
-var cell_situation = "not_checked" 
+var cell_situation = "not_checked"
 
 var user_blocks = document.currentScript.getAttribute('blocks');
 var user_timetables = document.currentScript.getAttribute('timetables');
 var timeslot_minutes = document.currentScript.getAttribute('variation_minutes');
+var max_quantity_cells = document.currentScript.getAttribute('max_quantity_cells');
+var limited_hours_passed;
+var checkboxes = [];
 
 $(document).ready(function() {
   console.log("timeslot minutos", timeslot_minutes);
@@ -60,10 +63,34 @@ $(document).ready(function() {
 
   // Pegar dados dos checkboxes
   $('.checkbox').click(function() {
+
+    var input_id = $(this).attr('for');
+    var [input_day_of_week, input_period, input_timeslot] = input_id.split('-');
+
+    checkboxes = $('input[type="checkbox"]:checked').filter('[id^='+input_day_of_week+']');
+
+    if (checkboxes.filter('[id^="mon"]').length > 9 ||
+        checkboxes.filter('[id^="tue"]').length > 9 ||
+        checkboxes.filter('[id^="wed"]').length > 9 ||
+        checkboxes.filter('[id^="thu"]').length > 9 ||
+        checkboxes.filter('[id^="fri"]').length > 9 ||
+        checkboxes.filter('[id^="sat"]').length > 9) {
+      limited_hours_passed = true;
+      $('#error-alert-form').text('A seleção da disponibilidade de horário não pode ultrapassar 8 horas de trabalho diárias.');
+      $('#error-alert-form').show();
+      window.scrollTo({
+        top: $('#error-alert-form').offset().top - $('.navbar').outerHeight() - 30,
+        behavior: 'smooth'
+      });
+    } else {
+      limited_hours_passed = false;
+      $('#error-alert-form').hide();
+    }
+    console.log(checkboxes.length);
+
     if(cell_left_number == 0 && cell_type_choosed == 0) {
       block_options();
     } else {
-      var input_id = $(this).attr('for');
       var input_val = $('#' + input_id).val();
       var button_is_checked = $('#' + input_id).prop('checked');
 
@@ -88,7 +115,7 @@ $(document).ready(function() {
         var index = timeslots.findIndex(function(lesson) {
           return lesson.id === input_id;
         });
-        
+
         if (index !== -1) {
           timeslots.splice(index, 1);
         }
@@ -134,7 +161,8 @@ $(document).ready(function() {
 
     if (user_regime && timeslots.length !== 0) {
       if(cell_left_number <= 0) {
-        $.ajax({
+        if(!limited_hours_passed) {
+          $.ajax({
           type: 'post',
           url: '/' + current_language + '/professor/preferencia-atribuicao/criar-fpa/editar-cursos/',
           data: {
@@ -156,8 +184,15 @@ $(document).ready(function() {
               top: $('#error-alert-form').offset().top - $('.navbar').outerHeight() - 30,
               behavior: 'smooth'
             });
+          }});
+          } else {
+            $('#error-message-form').text('Se certifique que a seleção de disponibilidade não ultrapassa 8 horas diárias de trabalho.');
+            $('#error-alert-form').show();
+            window.scrollTo({
+              top: $('#error-alert-form').offset().top - $('.navbar').outerHeight() - 30,
+              behavior: 'smooth'
+            });
           }
-        });
       } else {
         $('#error-message-form').text('Por favor insira todas as células.');
         $('#error-alert-form').show();
@@ -265,6 +300,6 @@ function update_cell_left_number(button_is_checked) {
         $('.cel-plus').css('display', 'none');
         $('.cel').css('display', '');
       }
-    }    
+    }
   }
 }
