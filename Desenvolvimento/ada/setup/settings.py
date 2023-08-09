d96859d88ab07699400e359052fc2ad910b29465
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-08-08 22:19:10 -0300

    feat: add 404 page error style part

--- Desenvolvimento/ada/setup/settings.py
+++ Desenvolvimento/ada/setup/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
