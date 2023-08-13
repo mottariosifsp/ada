1bdbd3553959c81319cd554118c0618370597e4f
Paulo Kenji <paulomuneischi@gmail.com>
2023-08-13 01:14:16 -0300

    feat: just open window with courses

--- Desenvolvimento/ada/templates/static/attribution/js/attribution_detail.js
+++ Desenvolvimento/ada/templates/static/attribution/js/attribution_detail.js
@@ -1,20 +1,23 @@
 var timetables_user = document.currentScript.getAttribute("timetables-user");
 var timetables_user = JSON.parse(timetables_user);
 
+$('.header-table').closest('table').find('.header-days').hide();
+$('.header-table').closest('table').find('tbody').hide();
+
 $.each(timetables_user, function(index, value) {
 
   let professor = value.professor;
   
   $("#cel-"+value.cord).html("<strong>" + value.acronym + "</strong>" + "<br>" + professor);
-  console.log(value.cord);
-  console.log(value.course);
+  $("#cel-"+value.cord).closest('table').find('.header-days').show();
+  $("#cel-"+value.cord).closest('table').find('tbody').show();
+  // console.log(value.cord);
+  // console.log(value.course);
 });
 
 $(document).ready(function() {
   // console.log(tametables_user)
   
-  $('.header-table').closest('table').find('.header-days').hide();
-  $('.header-table').closest('table').find('tbody').hide();
 
   $('.header-table').click(function() {
     $(this).find('.icon-minimize').text('-');
