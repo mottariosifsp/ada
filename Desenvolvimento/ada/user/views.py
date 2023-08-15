73c66ee6a6dbdc3d153f619f65e0d2b4039918a9
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-08-15 14:52:31 -0300

    feat: add log for 404 error page

--- Desenvolvimento/ada/user/views.py
+++ Desenvolvimento/ada/user/views.py
@@ -37,11 +37,32 @@ def logout_view(request):
     return redirect('login')
 
 def handler404(request, exception):
+
+    #MUDAR
+    log_directory = Path.home() / 'Documents' / 'logs'
+    log_directory.mkdir(parents=True, exist_ok=True)
+    log_file_path = log_directory / 'error.log'
+
+    custom_logger = logging.getLogger('custom_logger')
+    custom_handler = logging.FileHandler(log_file_path)
+    custom_handler.setFormatter(CustomFormatter())
+    custom_logger.addHandler(custom_handler)
+    custom_logger.setLevel(logging.ERROR)
+
+    user = request.user if hasattr(request, 'user') else None
+    if user and user.is_authenticated:
+        user_info = f'User: {user.first_name}'
+    else:
+        user_info = f'IP: {request.META["REMOTE_ADDR"]}' if hasattr(request, 'META') and 'REMOTE_ADDR' in request.META else 'IP: N/A'
+
+    custom_logger.error("Erro 404 - %s - Página não encontrada: %s", user_info, request.path)
+
     return render(request, 'error/404.html', status=404)
 
 
 def handler500(request):
-    # Configuração do logger personalizado
+
+    # MUDAR
     log_directory = Path.home() / 'Documents' / 'logs'
     log_directory.mkdir(parents=True, exist_ok=True)
     log_file_path = log_directory / 'error.log'
@@ -62,9 +83,7 @@ def handler500(request):
 
     custom_logger.error("Erro 500 - %s - Tipo de Exceção: %s, Mensagem: %s", user_info, exc_type.__name__, str(exc_value))
 
-    # Renderizar o template de erro
     return render(request, 'error/500.html', status=500)
-
 
 def provocar_erro_500(request):
     raise Exception("Erro 500 forçado - Internal Server Error")
