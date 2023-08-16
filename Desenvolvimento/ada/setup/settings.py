37177622b553352bab1fc994647cfd1ddc0ccd98
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-08-16 16:13:51 -0300

    fix: debug true

--- Desenvolvimento/ada/setup/settings.py
+++ Desenvolvimento/ada/setup/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 CSRF_TRUSTED_ORIGINS=['https://gusttavosoares-zany-palm-tree-95pjqq9x5vr2x6wv-8000.preview.app.github.dev']
