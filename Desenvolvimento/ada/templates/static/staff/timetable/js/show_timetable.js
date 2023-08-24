2b25c81e2ae56a755479220238b4f1c74a6a0c30
Paulo Kenji <paulomuneischi@gmail.com>
2023-08-24 15:24:12 -0300

    feat: add animation to window tables

--- Desenvolvimento/ada/templates/static/staff/timetable/js/show_timetable.js
+++ Desenvolvimento/ada/templates/static/staff/timetable/js/show_timetable.js
@@ -1,33 +1,51 @@
 var timetables_complete = document.currentScript.getAttribute("timetables-complete");
 var timetables_complete  = JSON.parse(timetables_complete);
 
-$('.header-table').closest('table').find('.header-days').hide();
-$('.header-table').closest('table').find('tbody').hide();
-
 $.each(timetables_complete, function(index, value) {
 
     $("#cel-"+value.cord).text(value.acronym);
 
-    $("#cel-"+value.cord).closest('table').find('.header-days').show();
-    $("#cel-"+value.cord).closest('table').find('tbody').show();
+    $("#cel-"+value.cord).closest('.content_collapsible').prev('.collapsible').addClass("default-open");
 
 });
 
 $(document).ready(function() {
   // console.log(tametables_user)
-  
 
-  $('.header-table').click(function() {
-    $(this).find('.icon-minimize').text('-');
-    $(this).closest('table').find('.header-days').toggle();
-    $(this).closest('table').find('tbody').toggle();
-    if ( $(this).closest('table').find('.header-days').is(":visible")) {
-      $('html, body').animate({
-        scrollTop: $(this).closest('table').offset().top - 100
-      }, 600);
+  // $('.header-table').click(function() {
+  //   $(this).find('.icon-minimize').text('-');
+  //   $(this).closest('table').find('.header-days').toggle();
+  //   $(this).closest('table').find('tbody').toggle();
+  //   if ( $(this).closest('table').find('.header-days').is(":visible")) {
+  //     $('html, body').animate({
+  //       scrollTop: $(this).closest('table').offset().top - 100
+  //     }, 600);
       
-    }else{
-      $(this).find('.icon-minimize').text('+');
-    }
-  });
+  //   }else{
+  //     $(this).find('.icon-minimize').text('+');
+  //   }
+  // });
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+
+  var content = coll[i].nextElementSibling;
+
+  if (coll[i].classList.contains("default-open")) {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active_collapse");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.scrollIntoView({ behavior: 'smooth' });
+    } 
+  });
+}
