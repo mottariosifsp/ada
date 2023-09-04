04492cd3b74c60e74c994437c94283c3b4a5f598
GusttavoSoares <gustavosoares335@yahoo.com.br>
2023-09-04 14:38:02 -0300

    style: assignments

--- Desenvolvimento/ada/professor/urls.py
+++ Desenvolvimento/ada/professor/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import home, profile, assignments, final_assignments_classs, show_assignment
+from .views import home, profile, assignments, assignments_classs_list, show_assignment
 
 urlpatterns = [
     path("", home, name="home_professor"),
     path("preferencia-atribuicao/", include("attribution_preference.urls")),
     path("perfil/", profile, name="profile"),
     path("atribuicoes/", assignments, name="assignments"),
-    path("atribuicoes/<str:name_block>/", final_assignments_classs, name='final_assignments_classs'),
+    path("atribuicoes/<str:name_block>/", assignments_classs_list, name='assignments_classs_list'),
     path("ver/", show_assignment, name='show_assignment'),
 ]
